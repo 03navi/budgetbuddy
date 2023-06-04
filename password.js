62c9cc571d5ec6e7ed2d45d34adb669869ff3bb7
Vaishnavi <90110510+03navi@users.noreply.github.com>
2023-06-05 00:20:00 +0530

    Update password.js

--- password.js
+++ password.js
@@ -1,6 +1,3 @@
-// Define the desired password requirements
-const passwordRegex = /^(?=.*[A-Z])(?=.*\d{2})(?=.*[@#$%^&+=_]).{9}$/;
-
 // Event: Login Form Submit
 document.querySelector('#loginForm').addEventListener('submit', (e) => {
   e.preventDefault();
@@ -9,18 +6,50 @@ document.querySelector('#loginForm').addEventListener('submit', (e) => {
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
 
-  // Validate credentials
-  if (password.match(passwordRegex)) {
-    // Redirect to the main finance tracker page upon successful login
-    window.location.href = 'index.html';
-  } else {
-    // Show error message or perform other actions for invalid credentials
-    alert('Invalid username or password. Please try again.');
-  }
+  // Check if the password reset form is visible
+  const resetFormVisible = document.querySelector('#resetForm').style.display !== 'none';
 
-  // Clear form fields
-  document.querySelector('#username').value = '';
-  document.querySelector('#password').value = '';
+  if (resetFormVisible) {
+    // Handle password reset
+    const newPassword = document.querySelector('#newPassword').value;
+    const confirmPassword = document.querySelector('#confirmPassword').value;
+
+    if (newPassword === confirmPassword && newPassword.match(passwordRegex)) {
+      // Perform password reset logic
+      alert('Password reset successful!');
+      document.querySelector('#resetForm').style.display = 'none';
+      document.querySelector('#loginForm').style.display = 'block';
+    } else {
+      // Show error message for invalid password reset
+      alert('Invalid password reset. Please try again.');
+    }
+
+    // Clear password reset form fields
+    document.querySelector('#newPassword').value = '';
+    document.querySelector('#confirmPassword').value = '';
+  } else {
+    // Validate credentials
+    if (password.match(passwordRegex)) {
+      // Redirect to the main finance tracker page upon successful login
+      window.location.href = 'index.html';
+    } else {
+      // Show error message or perform other actions for invalid credentials
+      alert('Invalid username or password. Please try again.');
+    }
+
+    // Clear login form fields
+    document.querySelector('#username').value = '';
+    document.querySelector('#password').value = '';
+  }
+});
+
+// Event: Reset Password Link Click
+document.querySelector('#resetPasswordLink').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  // Hide login form and show password reset form
+  document.querySelector('#loginForm').style.display = 'none';
+  document.querySelector('#resetForm').style.display = 'block';
 });
 
 
